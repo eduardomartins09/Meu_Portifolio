23077e5c3a4fb65ed73b0e1578417feb8b40ef11
Eduardo Martins <edu.ms2254@gmail.com>
2023-05-12 17:35:10 -0300

    adicionando menu hamburguer

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const btn = document.querySelector("#back-to-top")
 btn.addEventListener("click", () => {
     window.scrollTo(0, 0)
 })
+
+function menuShow() {
+    let menuMobile = document.querySelector('header .mobile-menu')
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = "imgs/close_white_36dp.svg"
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "imgs/menu_white_36dp.svg"
+    }
+}
